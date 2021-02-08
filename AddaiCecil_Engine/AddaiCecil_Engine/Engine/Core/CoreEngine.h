33f8733cf542e5076a99185ed1d47df0adb8d1e0
Pyromance14 <45087708+Pyromance14@users.noreply.github.com>
2021-02-08 17:16:10 -0500

    Add files via upload

---
+++ AddaiCecil_Engine/AddaiCecil_Engine/Engine/Core/CoreEngine.h
@@ -0,0 +1,52 @@
+#ifndef COREENGINE_H
+#define COREENGINE_H
+
+#include "Window.h"
+#include <memory>
+#include "Timer.h"
+#include "Debug.h"
+#include "GameInterface.h"
+#include "Scene.h"
+
+class CoreEngine{
+public:
+	CoreEngine(const CoreEngine&) = delete;
+	CoreEngine(CoreEngine&&) = delete;
+	CoreEngine& operator=(const CoreEngine&) = delete;
+	CoreEngine& operator=(CoreEngine&&) = delete;
+
+	static CoreEngine* GetInstance();
+
+
+	bool OnCreate(std::string name_, int width_, int height_);
+	void Run();
+	void Exit();
+
+	bool GetIsRunning() const;
+	int GetCurrentScene() const;
+
+	void SetGameInterface(GameInterface* gameInterface_);
+	void SetCurrentScene(int sceneNum_);
+
+private:
+	CoreEngine();
+	~CoreEngine();
+	void Update(const float deltaTime_);
+	void Render();
+	void OnDestroy();
+	
+	static std::unique_ptr<CoreEngine> engineInstance;
+	friend std::default_delete<CoreEngine>;
+
+	Window* window;
+	bool isRunning;
+	Timer timer;
+	unsigned int fps;
+
+	GameInterface* gameInterface;
+
+	int currentSceneNum;
+};
+
+#endif //!COREENGINE_H
+
